b3a9d9b02251c99882bef253ee3a22b795a1ba7a
codewithharsharma2004 <harshsharmaaaa04@gmail.com>
2025-12-31 00:59:38 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: "https://usermanagement-backend-b4ht.onrender.com"
 });
 
 api.interceptors.request.use(config => {
